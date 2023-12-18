9588aa3bf371324b1b10f4aa59cbf204992564c9
Yanuar Fa <yanuarfatur41@gmail.com>
2023-12-18 14:18:13 +0700

    edit some files

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-require("dotenv").config();
+// console.log(process.env);
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,15 +19,28 @@ app.use(cors());
 app.use(express.urlencoded({ limit: "50mb", extended: false }));
 app.use(cookieParser());
 
-db.mongoose
-  .connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Successfully connected to the database!");
-  })
-  .catch((err) => {
-    console.log("Failed when connecting to the database!", err);
-    process.exit();
-  });
+const connectDB = async () => {
+  try {
+    const conn = await db.mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+// db.mongoose
+//   .connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log("Successfully connected to the database!");
+//   })
+//   .catch((err) => {
+//     console.log("Failed when connecting to the database!", err);
+//     process.exit();
+//   });
 
 // app.get("/api/*", checkUser);
 require("./src/routes/index.routes")(app);
@@ -33,6 +49,8 @@ require("./src/routes/doctor.routes")(app);
 require("./src/routes/user.routes")(app);
 require("./src/routes/auth.routes")(app);
 
-app.listen(port, () => {
-  console.log(`Running on port http://localhost:${port}`);
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Running on port http://localhost:${port}`);
+  });
 });
